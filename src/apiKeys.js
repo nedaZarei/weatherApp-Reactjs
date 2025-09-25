a2abd1d997e9691db5160150895dc35edf934ada
neda.z <neda.z2003@gmail.com>
2025-09-26 00:03:59 +0330

    initial setup

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,5 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-  key: "{Your API Key Here}",
+  key: "a764b636a020bd9e52e94b1d51cedec8",
   base: "https://api.openweathermap.org/data/2.5/",
 };
